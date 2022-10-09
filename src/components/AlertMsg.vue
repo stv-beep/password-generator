04330ff8d250635f1e7578d8afe92a971d6c0ad6
stv-beep <aleixalguero0@gmail.com>
2022-10-09 15:55:43 +0200

    changes in alert style and hover

--- src/components/AlertMsg.vue
+++ src/components/AlertMsg.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="title-stv-beep content-center bg-green-300 font-bold justify-center items-center rounded-xl mx-2 my-6 py-3 text-green-900 hover:text-green-800 
+  <div id="alert-stv-beep" class="title-stv-beep content-center bg-green-300 font-bold justify-center items-center rounded-xl mx-2 my-6 py-3 text-green-900 hover:text-green-800 
 	border-4 border-zinc-800 m-9">{{ message }}
   </div>
 </template>
@@ -12,3 +12,18 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+#alert-stv-beep {
+  opacity: 1;
+  animation: fadeOut 3s;
+}
+@keyframes fadeOut {
+  from { opacity: 1; }
+  to { opacity: 0; }
+}
+@keyframes fadeIn {
+  from { opacity: 0; }
+  to { opacity: 1; }
+}
+</style>
